3dd13361444f457ade44246654ddd417e35b5c10
Dmitry Lisitsyn <polcertaain@gmail.com>
2023-02-23 21:16:33 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n)
+    n = String(n)
+  let num = Array.from(n)
+  let str = num.reverse().toString()
+  let result = ''
+  for (let i = 0;i < str.length;i++) {
+    if(str[i] !== ',') {
+      result += str[i]
+    }
+  }
+  return result
 }
